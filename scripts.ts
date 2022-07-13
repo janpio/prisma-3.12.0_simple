f16be950b23d6bae43a3a609690b011d779c0fd2
Jan Piotrowski <piotrowski+github@gmail.com>
2022-07-14 00:07:45 +0200

    uncommen things

--- scripts.ts
+++ scripts.ts
@@ -1,9 +1,9 @@
 import { PrismaClient as PrismaClientDataproxy } from './prisma/dataproxy'
 import { PrismaClient as PrismaClientLibrary } from './prisma/library'
 
-// const prismaLibrary = new PrismaClientLibrary({
-//   log: ['query', 'info', 'warn', 'error']
-// })
+const prismaLibrary = new PrismaClientLibrary({
+  log: ['query', 'info', 'warn', 'error']
+})
 const prismaDataproxy = new PrismaClientDataproxy({
   log: ['query', 'info', 'warn', 'error'],
   datasources: {
@@ -16,8 +16,8 @@ const prismaDataproxy = new PrismaClientDataproxy({
 async function main() {
 
   
-  // const foo2 = await prismaLibrary.foo.findMany({})
-  // console.log('foo library', foo2)
+  const foo2 = await prismaLibrary.foo.findMany({})
+  console.log('foo library', foo2)
 
   const foo = await prismaDataproxy.foo.findMany({})
   console.log('foo proxy', foo)
